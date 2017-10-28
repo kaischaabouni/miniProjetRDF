3a123647c27585c7b93055a73ba3227e78575c96
Kaïs Chaâbouni <kais9chaabouni@gmail.com>
2017-10-29 01:39:41 +0200

    Query

--- src/com/rdfengine/RDFEngine.java
+++ src/com/rdfengine/RDFEngine.java
@@ -1,5 +1,7 @@
 package com.rdfengine;
 
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
 import com.rdfengine.datasetconstruction.Loader;
 import com.rdfengine.query.QueryManager;
 
@@ -45,10 +47,14 @@ public class RDFEngine {
 		 *  Execute Queries: search for the result in columns
 		 */
 		// Suppose we have the query: SELECT ?v0 WHERE { ?v0 <http://schema.org/nationality> <http://db.uwaterloo.ca/~galuc/wsdbm/Country3> . } 
-		// subject: variable
-		// predicate: <http://schema.org/nationality>
-		// object: <http://db.uwaterloo.ca/~galuc/wsdbm/Country3>
-		QueryManager.executeQuery("Mock Query just for test");
+		String q = "SELECT ?v0 WHERE { "
+			+ "?v0 <http://purl.org/dc/terms/Location> <http://db.uwaterloo.ca/~galuc/wsdbm/City62> . "
+			+ "	?v0 <http://schema.org/nationality> <http://db.uwaterloo.ca/~galuc/wsdbm/Country29> . "
+			+ "	?v0 <http://db.uwaterloo.ca/~galuc/wsdbm/gender> <http://db.uwaterloo.ca/~galuc/wsdbm/Gender1> . "
+			+ "	?v0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://db.uwaterloo.ca/~galuc/wsdbm/Role2> .} ";
+
+		Query query = QueryFactory.create(q);
+		QueryManager.executeQuery(query);
 		long endQueryTime = System.currentTimeMillis();
 		
 		System.out.println("Loading Dataset Time : " + ((endLoadTime - startTime) / 1000) + " s");
--- src/com/rdfengine/query/QueryManager.java
+++ src/com/rdfengine/query/QueryManager.java
@@ -1,14 +1,44 @@
 package com.rdfengine.query;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.sparql.core.TriplePath;
+import com.hp.hpl.jena.sparql.syntax.ElementPathBlock;
+import com.hp.hpl.jena.sparql.syntax.ElementVisitorBase;
+import com.hp.hpl.jena.sparql.syntax.ElementWalker;
 import com.rdfengine.datasetconstruction.Triplets;
 import com.rdfengine.models.RDFDictionary;
 import com.rdfengine.models.Triplet;
 
 public class QueryManager {
 
-	public static void executeQuery(String string) {
+	/*
+	 * Execute Queries in files in directory
+	 */
+	public static void executeQuery (String queriesDirectoryPath){
+		
+	}
+	
+	/*
+	 * Execute Query (star query)
+	 */
+	public static void executeQuery(Query query) {
+		ElementWalker.walk(query.getQueryPattern(),new ElementVisitorBase(){
+			@Override public void visit(ElementPathBlock elementPathBlock){
+				ListIterator<TriplePath> listIterator=elementPathBlock.getPattern().iterator();
+				while (listIterator.hasNext()) {
+					TriplePath triplePath=listIterator.next();
+					executeTriple(triplePath);
+				}
+			}
+		});
+		
+		
+		/*
 		RDFDictionary dictionary = RDFDictionary.getInstance(); 
 		int idPredicate = dictionary.getId("<http://schema.org/nationality>");
 		int idObject = dictionary.getId("<http://db.uwaterloo.ca/~galuc/wsdbm/Country3>");
@@ -19,11 +49,17 @@ public class QueryManager {
 			}
 		}
 		
+		
 		// Print result
 		System.out.println("Query Result: ");	
 		for(Triplet triplet : resultQuery){
 			System.out.println(dictionary.getResource(triplet.getSubject()));	
 		}
+		*/
+	}
+
+	private static void executeTriple(TriplePath tp) {
+		System.out.println("S: " + tp.getSubject().getName() + " | P: " + tp.getPredicate().toString() + " | O: " + tp.getObject().toString());
 	}
 
 }
