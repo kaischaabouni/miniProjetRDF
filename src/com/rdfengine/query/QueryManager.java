4fd77cd2fc1f035e1e9d1e04e0e74806194c75ce
Kaïs Chaâbouni <kais9chaabouni@gmail.com>
2017-11-02 00:42:41 +0100

    Execute Query

--- src/com/rdfengine/query/QueryManager.java
+++ src/com/rdfengine/query/QueryManager.java
@@ -1,65 +1,96 @@
 package com.rdfengine.query;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.ListIterator;
-
-import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.sparql.core.TriplePath;
 import com.hp.hpl.jena.sparql.syntax.ElementPathBlock;
 import com.hp.hpl.jena.sparql.syntax.ElementVisitorBase;
 import com.hp.hpl.jena.sparql.syntax.ElementWalker;
 import com.rdfengine.datasetconstruction.Triplets;
-import com.rdfengine.models.RDFDictionary;
+import com.rdfengine.models.Dictionary;
 import com.rdfengine.models.Triplet;
 
 public class QueryManager {
 
 	/*
+	 * Temporary or Final Result
+	 */
+	private static ArrayList<Integer> queryResult = null;
+
+	/*
 	 * Execute Queries in files in directory
 	 */
 	public static void executeQuery (String queriesDirectoryPath){
-		
+
 	}
-	
+
 	/*
 	 * Execute Query (star query)
 	 */
 	public static void executeQuery(Query query) {
+
+		// initiate result to null
+		queryResult = null;
+
+		// Parsing the query and executing triple patterns
 		ElementWalker.walk(query.getQueryPattern(),new ElementVisitorBase(){
 			@Override public void visit(ElementPathBlock elementPathBlock){
 				ListIterator<TriplePath> listIterator=elementPathBlock.getPattern().iterator();
 				while (listIterator.hasNext()) {
 					TriplePath triplePath=listIterator.next();
-					executeTriple(triplePath);
+					executeTriplePatternWithJoin(triplePath, queryResult);
 				}
 			}
 		});
-		
-		
-		/*
-		RDFDictionary dictionary = RDFDictionary.getInstance(); 
-		int idPredicate = dictionary.getId("<http://schema.org/nationality>");
-		int idObject = dictionary.getId("<http://db.uwaterloo.ca/~galuc/wsdbm/Country3>");
-		ArrayList<Triplet> resultQuery = new ArrayList<Triplet>();
-		for(Triplet triplet :Triplets.getTriplets()){
-			if(triplet.getPredicate() == idPredicate && triplet.getObject() == idObject){
-				resultQuery.add(triplet);
+	}
+
+	/*
+	 * Execute a triple pattern
+	 */
+	private static void executeTriplePatternWithJoin(TriplePath tp, ArrayList<Integer> joinTable) {
+		Dictionary dictionary = Dictionary.getInstance();
+		int predicateID;
+		int objectID;
+
+		// Add <> if resource or "" if literal
+		String predicate = "<" + tp.getPredicate().toString() + ">";
+		String object;
+		if(tp.getObject().isLiteral()){
+			object = "\"" + tp.getObject().toString() + "\"";
+		} else {
+			object = "<" + tp.getObject().toString() + ">";
+		}
+
+		predicateID = dictionary.getId(predicate);
+		objectID = dictionary.getId(object);
+
+		queryResult = new ArrayList<Integer>();
+
+		for(Triplet triple :Triplets.getTriplets()){
+			if((joinTable == null || joinTable.contains(triple.getSubject())) 
+					&& triple.getPredicate() == predicateID 
+					&& triple.getObject() == objectID){
+				
+				// Add the subject ID if it is not already added
+				if(! queryResult.contains(triple.getSubject())){
+					queryResult.add(triple.getSubject());
+				}
 			}
 		}
-		
-		
-		// Print result
-		System.out.println("Query Result: ");	
-		for(Triplet triplet : resultQuery){
-			System.out.println(dictionary.getResource(triplet.getSubject()));	
+	}
+	
+	/*
+	 * Display Query Result
+	 */
+	public static void displayQueryResult(){
+		Dictionary dictionary = Dictionary.getInstance();
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("| Result                                                      |");
+		System.out.println("===============================================================");
+		for(int subjectID : queryResult){
+			System.out.println(dictionary.getResource(subjectID));
 		}
-		*/
+		System.out.println("---------------------------------------------------------------");
 	}
-
-	private static void executeTriple(TriplePath tp) {
-		System.out.println("S: " + tp.getSubject().getName() + " | P: " + tp.getPredicate().toString() + " | O: " + tp.getObject().toString());
-	}
-
 }
