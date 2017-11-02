48b86cb1095adacf1e3896fb15d695625ff9179b
Kaïs Chaâbouni <kais9chaabouni@gmail.com>
2017-11-02 01:39:12 +0100

    Execute queries from directory

--- src/com/rdfengine/RDFEngine.java
+++ src/com/rdfengine/RDFEngine.java
@@ -1,7 +1,5 @@
 package com.rdfengine;
 
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryFactory;
 import com.rdfengine.datasetconstruction.Loader;
 import com.rdfengine.query.QueryManager;
 
@@ -13,18 +11,15 @@ public class RDFEngine {
 	public static void main(String[] args) {
 
 		/*
-		 *  Default filePath and queriesDirecoryPath if RDFEngine executed without arguments
+		 *  Dataset filePath and queriesDirecoryPath
 		 */
 		String filePath = "assets/datasets/500K.rdf";  
 		String queriesDirecoryPath = "assets/queries/";  
-
-		/*
-		 *  Executing with Command Line Arguments 
-		 */
-		if(args.length > 0){
+		if(args.length == 2){
 			filePath = args[0];
 			queriesDirecoryPath = args[1];
 		}
+		
 
 		/*
 		 *  Load data to Dictionary from specified file
@@ -32,32 +27,13 @@ public class RDFEngine {
 		long startTime = System.currentTimeMillis();
 		Loader.loadData(filePath);
 		long endLoadTime = System.currentTimeMillis();
-
-		/*
-		 *  Show all Resources for test
-		 */
-		//RDFDictionary.getInstance().showAll(); 
-		//System.out.println(Triplets.getTriplets());
-
-		/*
-		 *  Parse SPARQL query
-		 */
-		
-		/*
-		 *  Execute Queries: search for the result in columns
-		 */
-		// Suppose we have the query: SELECT ?v0 WHERE { ?v0 <http://schema.org/nationality> <http://db.uwaterloo.ca/~galuc/wsdbm/Country3> . } 
-		String q = "SELECT ?v0 WHERE { "
-			+ "?v0 <http://purl.org/dc/terms/Location> <http://db.uwaterloo.ca/~galuc/wsdbm/City62> . "
-			+ "	?v0 <http://schema.org/nationality> <http://db.uwaterloo.ca/~galuc/wsdbm/Country29> . "
-			+ "	?v0 <http://db.uwaterloo.ca/~galuc/wsdbm/gender> <http://db.uwaterloo.ca/~galuc/wsdbm/Gender1> . "
-			+ "	?v0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://db.uwaterloo.ca/~galuc/wsdbm/Role2> .} ";
-
-		Query query = QueryFactory.create(q);
-		QueryManager.executeQuery(query);
-		long endQueryTime = System.currentTimeMillis();
-		
 		System.out.println("Loading Dataset Time : " + ((endLoadTime - startTime) / 1000) + " s");
+		
+		/*
+		 *  Execute Queries
+		 */
+		QueryManager.executeQueriesFromDirectoryPath(queriesDirecoryPath);
+		long endQueryTime = System.currentTimeMillis();
 		System.out.println("Query and Display Time : " + ((endQueryTime - endLoadTime) / 1000) + " s");
 	}
 
--- src/com/rdfengine/query/QueryManager.java
+++ src/com/rdfengine/query/QueryManager.java
@@ -1,8 +1,12 @@
 package com.rdfengine.query;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.ListIterator;
 import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.sparql.core.TriplePath;
 import com.hp.hpl.jena.sparql.syntax.ElementPathBlock;
 import com.hp.hpl.jena.sparql.syntax.ElementVisitorBase;
@@ -21,8 +25,33 @@ public class QueryManager {
 	/*
 	 * Execute Queries in files in directory
 	 */
-	public static void executeQuery (String queriesDirectoryPath){
+	public static void executeQueriesFromDirectoryPath (String queriesDirectoryPath){
+		
+		// Execute Queries from each file in the directory
+		File directory = new File(queriesDirectoryPath); 
+		for (File file : directory.listFiles()) {
+			try {
+				BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+				String line = null;
+				String sparqlQuery = "";
+				Query query;
+				while ((line = bufferedReader.readLine()) != null) {
+					if(! line.isEmpty()){
+						sparqlQuery = sparqlQuery + line;
+					} else {
+						if(! sparqlQuery.isEmpty()){
+							query = QueryFactory.create(sparqlQuery);
+							sparqlQuery = "";
+							executeQuery(query);
+						}
+					}
+				}
+				bufferedReader.close();
 
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/*
@@ -43,6 +72,8 @@ public class QueryManager {
 				}
 			}
 		});
+		
+		displayQueryResult();
 	}
 
 	/*
@@ -71,7 +102,7 @@ public class QueryManager {
 			if((joinTable == null || joinTable.contains(triple.getSubject())) 
 					&& triple.getPredicate() == predicateID 
 					&& triple.getObject() == objectID){
-				
+
 				// Add the subject ID if it is not already added
 				if(! queryResult.contains(triple.getSubject())){
 					queryResult.add(triple.getSubject());
@@ -79,7 +110,7 @@ public class QueryManager {
 			}
 		}
 	}
-	
+
 	/*
 	 * Display Query Result
 	 */
